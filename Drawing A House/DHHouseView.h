a00b81655a119c7016c56228197ddbf126514da6
gary g <tintin2000@yahoo.com>
2013-11-14 14:31:41 -0500

    first commit

---
+++ Drawing A House/DHHouseView.h
@@ -0,0 +1,13 @@
+//
+//  DHHouseView.h
+//  Drawing A House
+//
+//  Created by me on 11/14/13.
+//  Copyright (c) 2013 me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHHouseView : UIView
+
+@end
---
+++ Drawing A House/DHHouseView.m
@@ -0,0 +1,31 @@
+//
+//  DHHouseView.m
+//  Drawing A House
+//
+//  Created by me on 11/14/13.
+//  Copyright (c) 2013 me. All rights reserved.
+//
+
+#import "DHHouseView.h"
+
+@implementation DHHouseView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
